609c0fd1cb3078e76b2fae0724e1d20d168d5949
Derek Rush <drush81@gmail.com>
2018-08-11 02:48:30 -0700

    test

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -3,7 +3,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var MiniCssExtractPlugin = require('mini-css-extract-plugin')
 var ManifestPlugin = require('webpack-manifest-plugin');
 // var InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
-var url = require('url');
+// var url = require('url');
 var paths = require('./paths');
 var getClientEnvironment = require('./env');
 
@@ -14,7 +14,7 @@ var getClientEnvironment = require('./env');
 var publicPath = paths.servedPath;
 // Some apps do not use client-side routing with pushState.
 // For these, "homepage" can be set to "." to enable relative asset paths.
-var shouldUseRelativeAssetPaths = publicPath === './';
+// var shouldUseRelativeAssetPaths = publicPath === './';
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_URL%/xyz looks better than %PUBLIC_URL%xyz.
